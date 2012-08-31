e29a846f122eb81c85a0af3334e42e9b66350fb6
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2012-08-31 09:21:46 +0900

    Add spec for #new

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,7 +1,20 @@
 require 'postfix_admin/base'
 
 describe PostfixAdmin::Base do
-  it "new" do
+  before do
+    @base = PostfixAdmin::Base.new({'database' => 'mysql://postfix:password@localhost/postfix'})
+  end
+
+  it "#new without config" do
     lambda { PostfixAdmin::Base.new }.should raise_error(ArgumentError)
   end
+
+  it "#new without database config" do
+    lambda { PostfixAdmin::Base.new({}) }.should raise_error(ArgumentError)
+  end
+
+  it "#new with database config" do
+    lambda { PostfixAdmin::Base.new({'database' => 'mysql://postfix:password@localhost/postfix'}) }.should_not raise_error
+  end
+
 end
