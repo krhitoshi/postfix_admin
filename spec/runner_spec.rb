392a61d9b42239454564b6a4974845d8e7e9c037
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2012-09-19 08:57:52 +0900

    Add spec for runner

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -1,7 +1,14 @@
+require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 require 'postfix_admin/runner'
 
 describe PostfixAdmin::Runner do
-  it "start" do
-    1.should == 1
+  describe "#show" do
+    it "#show shows information of example.com" do
+      capture(:stdout){ PostfixAdmin::Runner.start(["show"]) }.should =~ /example.com.+100.+100.+100/
+    end
+
+    it "#show shows information of admin@example.com" do
+      capture(:stdout){ PostfixAdmin::Runner.start(["show"]) }.should =~ /admin@example.com.+password/
+    end
   end
 end
