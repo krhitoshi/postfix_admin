220be975559d9a1e652d61e8a9497447f28f2f74
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2012-09-19 08:56:57 +0900

    Add spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+RSpec.configure do |config|
+  config.before do
+    ARGV.replace []
+  end
+
+  def capture(stream)
+    begin
+      stream = stream.to_s
+      eval "$#{stream} = StringIO.new"
+      yield
+      result = eval("$#{stream}").string
+    ensure
+      eval("$#{stream} = #{stream.upcase}")
+    end
+
+    result
+  end
+
+  def source_root
+    File.join(File.dirname(__FILE__), 'fixtures')
+  end
+
+  def destination_root
+    File.join(File.dirname(__FILE__), 'sandbox')
+  end
+
+  alias :silence :capture
+end
