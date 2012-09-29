c3ca7ee99afe4141a7ce1b17b8d3ca10ddfe78c7
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2012-09-30 05:52:47 +0900

    Fix format

--- lib/postfix_admin/base.rb
+++ lib/postfix_admin/base.rb
@@ -43,6 +43,7 @@ class PostfixAdmin::Base
     d_admin.domains << d_domain
     d_admin.save or raise "Error: Relation Error"
   end
+
   def add_admin(username, password)
     if admin_exist?(username)
       raise "Error: #{username} is already resistered as admin."
@@ -56,6 +57,7 @@ class PostfixAdmin::Base
     }
     admin.save
   end
+
   def add_account(address, password)
     if address !~ /.+\@.+\..+/
       raise "Error: Invalid mail address! #{address}"
@@ -94,6 +96,7 @@ class PostfixAdmin::Base
     }
     mailbox.save
   end
+
   def add_alias(address, goto)
     if alias_exist?(address)
       goto_text = "#{address},#{goto}"
@@ -103,6 +106,7 @@ class PostfixAdmin::Base
       raise "Error: Invalid mail address! #{address}"
     end
   end
+
   def add_domain(domain_name)
     if domain_name !~ /.+\..+/
       raise "Error: Ivalid domain! #{domain_name}"
@@ -122,6 +126,7 @@ class PostfixAdmin::Base
     }
     domain.save
   end
+
   def delete_domain(domain)
     unless domain_exist?(domain)
       raise "Error: #{domain} is not found!"
@@ -148,24 +153,31 @@ class PostfixAdmin::Base
   def delete_unnecessary_admins
     PostfixAdmin::Admin.unnecessary.destroy or raise "Error: Cannnot destroy Admin"
   end
+
   def admin_domain_exist?(username, domain)
     PostfixAdmin::DomainAdmin.all(:username => username, :domain => domain).count != 0
   end
+
   def admin_exist?(admin)
     PostfixAdmin::Admin.all(:username => admin).count != 0
   end
+
   def alias_exist?(address)
     PostfixAdmin::Alias.all(:address => address).count != 0
   end
+
   def domain_exist?(domain)
     PostfixAdmin::Domain.all(:domain => domain).count != 0
   end
+
   def domains
     PostfixAdmin::Domain.all(:domain.not => 'ALL', :order => :domain)
   end
+
   def admins
     PostfixAdmin::Admin.all(:order => 'username')
   end
+
   def mailboxes(domain=nil)
     if domain
       PostfixAdmin::Mailbox.all(:domain => domain, :order => :username)
@@ -173,6 +185,7 @@ class PostfixAdmin::Base
       PostfixAdmin::Mailbox.all(:order => :username)
     end
   end
+
   def aliases(domain=nil)
     if domain
       PostfixAdmin::Alias.all(:domain => domain, :order => :address)
@@ -180,6 +193,7 @@ class PostfixAdmin::Base
       PostfixAdmin::Alias.all(:order => :address)
     end
   end
+
   def admin_domains(username=nil)
     if username
       PostfixAdmin::Admin.first(:username => username).domains
--- lib/postfix_admin/cli.rb
+++ lib/postfix_admin/cli.rb
@@ -31,9 +31,11 @@ class PostfixAdmin
       end
       print_line
     end
+
     def admin_exist?(admin)
       @admin.admin_exist?(admin)
     end
+
     def show_admin
       admins = @admin.admins
       if admins.count == 0
@@ -52,6 +54,7 @@ class PostfixAdmin
       end
       print_line
     end
+
     def show_domain_account(domain)
       mailboxes = @admin.mailboxes(domain)
       if mailboxes.count == 0
@@ -101,23 +104,29 @@ class PostfixAdmin
       end
       print_line
     end
+
     def add_domain(domain)
       @admin.add_domain(domain)
     end
+
     def add_admin(user_name, password)
       validate_password(password)
       @admin.add_admin(user_name, password)
     end
+
     def add_admin_domain(user_name, domain)
       @admin.add_admin_domain(user_name, domain)
     end
+
     def add_account(address, password)
       validate_password(password)
       @admin.add_account(address, password)
     end
+
     def add_alias(address, goto)
       @admin.add_alias(address, goto)
     end
+
     def delete_domain(domain)
       @admin.delete_domain(domain)
     end
@@ -142,15 +151,18 @@ class PostfixAdmin
         YAML.load(f.read)
       end
     end
+
     def create_config(config_file)
       open(config_file, 'w') do |f|
         f.write PostfixAdmin::Base::DEFAULT_CONFIG.to_yaml
       end
       File.chmod(0600, config_file)
     end
+
     def print_line
       puts "-"*100
     end
+
     def validate_password(password)
       if password.size < MIN_NUM_PASSWORD_CHARACTER
         raise "Error: Password is too short. It should be larger than #{MIN_NUM_PASSWORD_CHARACTER}"
