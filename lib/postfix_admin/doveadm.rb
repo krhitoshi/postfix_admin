644560242f7a0d89d7f099395c64348481e2d361
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2013-06-02 23:21:53 +0900

    shell escape argument words

--- lib/postfix_admin/doveadm.rb
+++ lib/postfix_admin/doveadm.rb
@@ -1,5 +1,6 @@
 
 require 'open3'
+require 'shellwords'
 
 module PostfixAdmin
   class Doveadm
@@ -8,7 +9,9 @@ module PostfixAdmin
       result.split
     end
 
-    def self.password(password, scheme)
+    def self.password(in_password, in_scheme)
+      password = Shellwords.escape(in_password)
+      scheme  = Shellwords.escape(in_scheme)
       stdin, stdout, stderr = Open3.popen3("doveadm pw -s #{scheme} -p #{password}")
       if stderr.readlines.to_s =~ /Fatal:/
         raise Error, stderr.readlines
