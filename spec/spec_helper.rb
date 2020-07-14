47c20be4469aec9d86ba879303b4ccba6a9b9083
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2020-07-14 12:46:32 +0900

    Updated spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,21 @@
 $:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
 require 'fileutils'
+require 'bundler/setup'
 require 'postfix_admin'
 require 'postfix_admin/cli'
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  # config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    # c.syntax = :expect
+  end
+end
 
 include PostfixAdmin
 
