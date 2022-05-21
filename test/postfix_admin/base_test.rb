648b4da78ea1b809ac9680508d72afdbb651e3d9
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2022-05-21 11:01:11 +0900

    Add tests

--- test/postfix_admin/base_test.rb
+++ test/postfix_admin/base_test.rb
@@ -5,6 +5,11 @@ require "active_support"
 require "postfix_admin"
 
 class BaseTest < ActiveSupport::TestCase
+  setup do
+    config = { "database" => "mysql2://postfix:password@localhost/postfix" }
+    @base = PostfixAdmin::Base.new(config)
+  end
+
   test "DEFAULT_CONFIG" do
     expect = {
       "database" => "mysql2://postfix:password@localhost/postfix",
@@ -15,5 +20,13 @@ class BaseTest < ActiveSupport::TestCase
       "passwordhash_prefix" => true
     }
     assert_equal expect, PostfixAdmin::Base::DEFAULT_CONFIG
+  end
+
+  test "#address_split" do
+    assert_equal %w[user example.com], @base.address_split('user@example.com')
+  end
+
+  test "#new without config" do
+    assert_raise(ArgumentError) { PostfixAdmin::Base.new }
   end
 end
