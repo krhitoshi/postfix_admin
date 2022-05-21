1c91170586c438e207fda86aff86dcc6c729acf2
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2022-05-21 11:01:55 +0900

    include PostfixAdmin for tests

--- test/postfix_admin/base_test.rb
+++ test/postfix_admin/base_test.rb
@@ -5,9 +5,11 @@ require "active_support"
 require "postfix_admin"
 
 class BaseTest < ActiveSupport::TestCase
+  include PostfixAdmin
+
   setup do
     config = { "database" => "mysql2://postfix:password@localhost/postfix" }
-    @base = PostfixAdmin::Base.new(config)
+    @base = Base.new(config)
   end
 
   test "DEFAULT_CONFIG" do
@@ -19,14 +21,14 @@ class BaseTest < ActiveSupport::TestCase
       "scheme" => "CRAM-MD5",
       "passwordhash_prefix" => true
     }
-    assert_equal expect, PostfixAdmin::Base::DEFAULT_CONFIG
+    assert_equal expect, Base::DEFAULT_CONFIG
   end
 
   test "#address_split" do
-    assert_equal %w[user example.com], @base.address_split('user@example.com')
+    assert_equal %w[user example.com], @base.address_split("user@example.com")
   end
 
   test "#new without config" do
-    assert_raise(ArgumentError) { PostfixAdmin::Base.new }
+    assert_raise(ArgumentError) { Base.new }
   end
 end
