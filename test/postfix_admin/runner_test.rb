9b875d7f1bd5d9163648dc837cdfe19108d532ea
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2022-05-24 09:43:43 +0900

    Runner#delete_admin deletes an Admin

--- test/postfix_admin/runner_test.rb
+++ test/postfix_admin/runner_test.rb
@@ -5,6 +5,7 @@ class RunnerTest < ActiveSupport::TestCase
   setup do
     db_reset
     @domain = create(:domain, domain: "example.test")
+    @domain.admins << build(:admin, username: "admin@example.test")
   end
 
   test "#add_domain adds a new Domain" do
@@ -30,5 +31,14 @@ class RunnerTest < ActiveSupport::TestCase
       assert_match '"admin@new-domain.test" was successfully registered as an admin', res
     end
     assert Admin.exists?("admin@new-domain.test")
+  end
+
+  test "#delete_admin deletes an Admin" do
+    assert Admin.exists?("admin@example.test")
+    assert_difference("Admin.count", -1) do
+      res = capture(:stdout) { Runner.start(%w[delete_admin admin@example.test]) }
+      assert_match '"admin@example.test" was successfully deleted', res
+    end
+    assert_not Admin.exists?("admin@example.test")
   end
 end
