26c9bc3479dd86bd9d54b5ece5216c2a6d2527ce
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2022-05-25 10:47:00 +0900

    Refactoring

--- test/postfix_admin/runner_test.rb
+++ test/postfix_admin/runner_test.rb
@@ -10,7 +10,7 @@ class RunnerTest < ActiveSupport::TestCase
 
   test "usual flow with add/delete methods" do
     assert_nothing_raised do
-      capture(:stdout) do
+      capture do
         Runner.start(%w[add_domain new-domain.test])
         Runner.start(%w[add_admin admin@new-domain.test password])
         Runner.start(%w[add_admin_domain admin@new-domain.test new-domain.test])
@@ -24,7 +24,7 @@ class RunnerTest < ActiveSupport::TestCase
 
   test "#add_domain adds a new Domain" do
     assert_difference("Domain.count") do
-      res = capture(:stdout) { Runner.start(%w[add_domain new-domain.test]) }
+      res = capture { Runner.start(%w[add_domain new-domain.test]) }
       assert_match '"new-domain.test" was successfully registered as a domain', res
     end
     assert Domain.exists?("new-domain.test")
@@ -33,7 +33,7 @@ class RunnerTest < ActiveSupport::TestCase
   test "#delete_domain deletes a Domain" do
     assert Domain.exists?("example.test")
     assert_difference("Domain.count", -1) do
-      res = capture(:stdout) { Runner.start(%w[delete_domain example.test]) }
+      res = capture { Runner.start(%w[delete_domain example.test]) }
       assert_match '"example.test" was successfully deleted', res
     end
     assert_not Domain.exists?("example.test")
@@ -41,7 +41,7 @@ class RunnerTest < ActiveSupport::TestCase
 
   test "#add_admin adds an Admin" do
     assert_difference("Admin.count") do
-      res = capture(:stdout) { Runner.start(%w[add_admin admin@new-domain.test password]) }
+      res = capture { Runner.start(%w[add_admin admin@new-domain.test password]) }
       assert_match '"admin@new-domain.test" was successfully registered as an admin', res
     end
     assert Admin.exists?("admin@new-domain.test")
@@ -50,7 +50,7 @@ class RunnerTest < ActiveSupport::TestCase
   test "#delete_admin deletes an Admin" do
     assert Admin.exists?("admin@example.test")
     assert_difference("Admin.count", -1) do
-      res = capture(:stdout) { Runner.start(%w[delete_admin admin@example.test]) }
+      res = capture { Runner.start(%w[delete_admin admin@example.test]) }
       assert_match '"admin@example.test" was successfully deleted', res
     end
     assert_not Admin.exists?("admin@example.test")
