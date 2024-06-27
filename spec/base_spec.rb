59ec1163511f7b4992539f536e6b7f2543c97dd0
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2024-06-27 12:00:13 +0900

    Turn tests into specs

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -7,6 +7,91 @@ RSpec.describe PostfixAdmin::Base do
     @base = Base.new({'database' => 'mysql2://postfix:password@localhost/postfix'})
   end
 
+  it "DEFAULT_CONFIG" do
+    expected = {
+      "database" => "mysql2://postfix:password@localhost/postfix",
+      "aliases" => 30,
+      "mailboxes" => 30,
+      "maxquota" => 100,
+      "scheme" => "CRAM-MD5",
+      "passwordhash_prefix" => true
+    }
+    expect(Base::DEFAULT_CONFIG).to eq(expected)
+  end
+
+  describe "#config" do
+    it "Default configurations to be correct" do
+      expect(@base.config[:aliases]).to eq(30)
+      expect(@base.config[:mailboxes]).to eq(30)
+      expect(@base.config[:maxquota]).to eq(100)
+      expect(@base.config[:scheme]).to eq("CRAM-MD5")
+      expect(@base.config[:passwordhash_prefix]).to be true
+    end
+
+    it "#config[:passwordhash_prefix]" do
+      expect(Base.new({}).config[:passwordhash_prefix]).to be true
+      expect(Base.new({"passwordhash_prefix" => true})
+                 .config[:passwordhash_prefix]).to be true
+      expect(Base.new({"passwordhash_prefix" => false})
+                 .config[:passwordhash_prefix]).to be false
+    end
+
+    it "#config[:database]" do
+      expect(@base.config[:database]).to eq("mysql2://postfix:password@localhost/postfix")
+    end
+  end
+
+  describe "#add_domain" do
+    it "can adds a new domain" do
+      expect {
+        @base.add_domain("new-domain.test")
+      }.to change { Domain.count }.by(1)
+      expect(Domain.exists?("new-domain.test")).to be true
+    end
+
+    it "raises an error for an existing domain" do
+      expect(Domain.exists?("example.com")).to be true
+      expect {
+        @base.add_domain("example.com")
+      }.to raise_error(PostfixAdmin::Error,
+                       "Domain has already been registered: example.com")
+      expect(Domain.count).to eq(Domain.count)
+    end
+
+    it "raises an error for an invalid domain" do
+      expect {
+        @base.add_domain("invalid_domain")
+      }.to raise_error(PostfixAdmin::Error, "Invalid domain name: invalid_domain")
+      expect(Domain.count).to eq(Domain.count)
+    end
+  end
+
+  describe "#delete_domain" do
+    it "deletes a domain" do
+      expect(Domain.exists?("example.com")).to be true
+      expect(Admin.exists?("admin@example.com")).to be true
+      expect(DomainAdmin.exists?(username: "admin@example.com", domain: "example.com")).to be true
+      expect(Alias.exists?(domain: "example.com")).to be true
+      expect(Mailbox.exists?(domain: "example.com")).to be true
+
+      expect {
+        @base.delete_domain("example.com")
+      }.to change { Domain.count }.by(-1)
+
+      expect(Domain.exists?("example.com")).to be false
+      expect(Admin.exists?("admin@example.com")).to be false
+      expect(DomainAdmin.exists?(username: "admin@example.com", domain: "example.com")).to be false
+      expect(Alias.exists?(domain: "example.com")).to be false
+      expect(Mailbox.exists?(domain: "example.com")).to be false
+    end
+
+    it "raises an error for a non-existent domain name" do
+      expect {
+        @base.delete_domain("non-existent.test")
+      }.to raise_error(PostfixAdmin::Error, "Could not find domain: non-existent.test")
+    end
+  end
+
   describe "#add_admin" do
     it "can add an new admin" do
       num_admins = Admin.count
@@ -65,6 +150,97 @@ RSpec.describe PostfixAdmin::Base do
 
     it "raise error when unknown domain" do
       expect { @base.delete_admin_domain('admin@example.com', 'unknown.example.com') }.to raise_error Error
+    end
+  end
+
+  describe "#add_account" do
+    it "adds a Mailbox and an Alias" do
+      expect {
+        @base.add_account("new_account@example.com", CRAM_MD5_PASS)
+      }.to change{ Mailbox.count }.by(1).and change{ Alias.count }.by(1)
+      expect(Mailbox.exists?("new_account@example.com")).to be true
+      expect(Alias.exists?("new_account@example.com")).to be true
+
+      domain = Domain.find("example.com")
+      expect(domain.rel_mailboxes.exists?("new_account@example.com")).to be true
+
+      mailbox = Mailbox.find("new_account@example.com")
+      expect(mailbox.name).to eq("")
+      expect(mailbox.username).to eq("new_account@example.com")
+      expect(mailbox.local_part).to eq("new_account")
+      expect(mailbox.maildir).to eq("example.com/new_account@example.com/")
+      expect(mailbox.password).to eq(CRAM_MD5_PASS)
+      expect(mailbox.quota).to eq(102_400_000)
+    end
+
+    context "with name" do
+      it "adds a Mailbox and an Alias with name" do
+        expect {
+          @base.add_account("john_smith@example.com", CRAM_MD5_PASS, name: "John Smith")
+        }.to change{ Mailbox.count }.by(1).and change{ Alias.count }.by(1)
+        mailbox_with_name = Mailbox.find("john_smith@example.com")
+        expect(mailbox_with_name.name).to eq("John Smith")
+      end
+    end
+
+    it "raises an error for an empty password" do
+      ["", nil].each do |empty_pass|
+        expect {
+          @base.add_account("new_account@example.com", empty_pass)
+        }.to raise_error(PostfixAdmin::Error, "Empty password")
+        expect(Mailbox.count).to eq(Mailbox.count)
+        expect(Alias.count).to eq(Alias.count)
+      end
+    end
+
+    it "raises an error for an invalid address" do
+      expect {
+        @base.add_account("invalid.example.com", "password")
+      }.to raise_error(PostfixAdmin::Error, "Invalid email address: invalid.example.com")
+      expect(Mailbox.count).to eq(Mailbox.count)
+      expect(Alias.count).to eq(Alias.count)
+    end
+
+    it "raises an error for a non-existent domain name" do
+      expect {
+        @base.add_account("user@unknown.example.com", "password")
+      }.to raise_error(PostfixAdmin::Error, "Could not find domain: unknown.example.com")
+      expect(Mailbox.count).to eq(Mailbox.count)
+      expect(Alias.count).to eq(Alias.count)
+    end
+
+    it "raises an error for an existing mailbox or an alias" do
+      expect {
+        @base.add_account("user@example.com", "password")
+      }.to raise_error(PostfixAdmin::Error, "Alias has already been registered: user@example.com")
+      expect(Mailbox.count).to eq(Mailbox.count)
+      expect(Alias.count).to eq(Alias.count)
+
+      expect {
+        @base.add_account("alias@example.com", "password")
+      }.to raise_error(PostfixAdmin::Error, "Alias has already been registered: alias@example.com")
+      expect(Mailbox.count).to eq(Mailbox.count)
+      expect(Alias.count).to eq(Alias.count)
+    end
+  end
+
+  describe "#delete_account" do
+    it "deletes a Mailbox and an Alias" do
+      expect(Alias.exists?("user@example.com")).to be true
+      expect(Mailbox.exists?("user@example.com")).to be true
+
+      expect {
+        @base.delete_account("user@example.com")
+      }.to change{ Mailbox.count }.by(-1).and change{ Alias.count }.by(-1)
+
+      expect(Alias.exists?("user@example.com")).to be false
+      expect(Mailbox.exists?("user@example.com")).to be false
+    end
+
+    it "raises an error for a non-existent account" do
+      expect {
+        @base.delete_account("unknown@example.com")
+      }.to raise_error(PostfixAdmin::Error, "Could not find account: unknown@example.com")
     end
   end
 
