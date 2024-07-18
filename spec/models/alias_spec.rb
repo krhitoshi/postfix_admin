333b200a2c05dc5262eecc22ab1a90495b83b593
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2024-07-18 23:10:08 +0900

    Add spec/models/alias_spec.rb

--- spec/models/alias_spec.rb
+++ spec/models/alias_spec.rb
@@ -1,8 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
-require 'postfix_admin/models/alias'
-require 'postfix_admin/models/domain_admin'
-require 'postfix_admin/models/log'
-require 'postfix_admin/models/quota2'
+require File.join(__dir__, "..", "spec_helper")
+require "postfix_admin/models/alias"
 
 RSpec.describe PostfixAdmin::Alias do
   it "active" do
